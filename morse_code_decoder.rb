080dae1fe4e1f89b86092b3906ffb8e7b0ecbcd9
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-11-02 16:58:07 -0300

    Separate code intro three methods

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -1,21 +1,27 @@
-def morse_code_decoder(cypher_string)
-  morse_hash = {
-    '.-' => 'a', '-...' => 'b', '-.-.' => 'c', '-..' => 'd', '.' => 'e', '..-.' => 'f',
-    '--.' => 'g', '....' => 'h', '..' => 'i', '.---' => 'j', '-.-' => 'k', '.-..' => 'l',
-    '--' => 'm', '-.' => 'n', '---' => 'o', '.--.' => 'p', '--.-' => 'q', '.-.' => 'r',
-    '...' => 's', '-' => 't', '..-' => 'u', '...-' => 'v', '.--' => 'w',
-    '-..-' => 'x', '-.--' => 'y', '--..' => 'z', '   ' => ' '
-  }
+MORSE_HASH = {
+  '.-' => 'a', '-...' => 'b', '-.-.' => 'c', '-..' => 'd', '.' => 'e', '..-.' => 'f',
+  '--.' => 'g', '....' => 'h', '..' => 'i', '.---' => 'j', '-.-' => 'k', '.-..' => 'l',
+  '--' => 'm', '-.' => 'n', '---' => 'o', '.--.' => 'p', '--.-' => 'q', '.-.' => 'r',
+  '...' => 's', '-' => 't', '..-' => 'u', '...-' => 'v', '.--' => 'w',
+  '-..-' => 'x', '-.--' => 'y', '--..' => 'z', '   ' => ' '
+}.freeze
 
-  # decode cypher text
-  cypher_array = cypher_string.split(/\s/).map { |i| morse_hash[i] }
-  text = ''
-  cypher_array.each do |t|
-    text += (t || ' ')
-  end
-  text.split.join(' ').upcase
+def morse_char_decoder(_input)
+  MORSE_HASH[_input].upcase
+end
+
+def morse_word_decoder(_input)
+  _output = ''
+  _input.split.each { |word| _output += morse_char_decoder(word)}
+  _output
+end
+
+def morse_sentence_decoder(_input)
+  _output = ''
+  _input.split('   ').each { |sentence|  _output += "#{morse_word_decoder(sentence)} "}
+  _output
 end
 
 # Puts cypher text
-puts(morse_code_decoder('-- -.--   -. .- -- .'))
-puts(morse_code_decoder('.-   -... --- -..-   ..-. ..- .-.. .-..          --- ..-.   .-. ..- -... .. . ...'))
+puts(morse_sentence_decoder('-- -.--   -. .- -- .'))
+puts(morse_sentence_decoder('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'))
